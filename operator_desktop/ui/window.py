27486a565d8c12d93dbe4ab140a7c83bba4db27e
veldtie <chalk_bro@vk.com>
2026-02-09 02:36:58 +0300

    window

--- operator_desktop/ui/window.py
+++ operator_desktop/ui/window.py
@@ -17,19 +17,10 @@ from .shell import MainShell
 class MainWindow(QtWidgets.QMainWindow):
     _DEFAULT_WINDOW_SIZE = QtCore.QSize(1280, 800)
     _MIN_WINDOW_SIZE = QtCore.QSize(1100, 700)
-    _RESIZE_MARGIN = 8
 
     def __init__(self, settings_path):
         super().__init__()
-        self._drag_active = False
-        self._drag_offset = QtCore.QPoint()
-        self._resize_active = False
-        self._resize_edges: str | None = None
-        self._resize_start_pos = QtCore.QPoint()
-        self._resize_start_geom = QtCore.QRect()
         self._last_normal_size: QtCore.QSize | None = None
-        self._chrome_buttons: list[QtWidgets.QToolButton] = []
-        self.chrome_bar: QtWidgets.QFrame | None = None
 
         self.settings = SettingsStore(settings_path)
         self.i18n = I18n(self.settings)
@@ -65,10 +56,16 @@ class MainWindow(QtWidgets.QMainWindow):
         shadow.setColor(QtGui.QColor(0, 0, 0, 155))
         self.window_frame.setGraphicsEffect(shadow)
 
+        self.header_row = QtWidgets.QFrame()
+        self.header_row.setObjectName("HeaderRow")
+        header_layout = QtWidgets.QHBoxLayout(self.header_row)
+        header_layout.setContentsMargins(6, 0, 6, 0)
+        header_layout.setSpacing(8)
         self.header_title = QtWidgets.QLabel()
         self.header_title.setObjectName("ChromeTitle")
-        self.chrome_bar = self._build_chrome_bar()
-        frame_layout.addWidget(self.chrome_bar)
+        header_layout.addWidget(self.header_title)
+        header_layout.addStretch()
+        frame_layout.addWidget(self.header_row)
 
         self.stack = QtWidgets.QStackedWidget()
         self.login_page = LoginPage(self.i18n, self.settings)
@@ -113,60 +110,13 @@ class MainWindow(QtWidgets.QMainWindow):
             self.header_title.setText(title)
 
     def _configure_window_chrome(self) -> None:
-        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint, True)
-        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_StyledBackground, True)
-        self._enable_mouse_tracking(self)
+        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint, False)
+        self.setWindowFlag(QtCore.Qt.WindowType.WindowMinimizeButtonHint, True)
+        self.setWindowFlag(QtCore.Qt.WindowType.WindowMaximizeButtonHint, True)
+        self.setWindowFlag(QtCore.Qt.WindowType.WindowCloseButtonHint, True)
         self.setMinimumSize(self._MIN_WINDOW_SIZE)
         self._apply_saved_window_size()
-        app = QtWidgets.QApplication.instance()
-        if app:
-            app.installEventFilter(self)
-
-    def _enable_mouse_tracking(self, widget: QtWidgets.QWidget) -> None:
-        widget.setMouseTracking(True)
-        for child in widget.findChildren(QtWidgets.QWidget):
-            child.setMouseTracking(True)
-
-    def _build_chrome_bar(self) -> QtWidgets.QFrame:
-        bar = QtWidgets.QFrame()
-        bar.setObjectName("ChromeBar")
-        bar.setAttribute(QtCore.Qt.WidgetAttribute.WA_StyledBackground, True)
-        layout = QtWidgets.QHBoxLayout(bar)
-        layout.setContentsMargins(12, 6, 12, 6)
-        layout.setSpacing(10)
-
-        controls = QtWidgets.QFrame(bar)
-        controls_layout = QtWidgets.QHBoxLayout(controls)
-        controls_layout.setContentsMargins(0, 0, 0, 0)
-        controls_layout.setSpacing(8)
-
-        self._close_button = self._make_chrome_button("close", self.close, "Close")
-        self._min_button = self._make_chrome_button("minimize", self.showMinimized, "Minimize")
-        self._max_button = self._make_chrome_button("zoom", self._toggle_maximize, "Maximize")
-
-        controls_layout.addWidget(self._close_button)
-        controls_layout.addWidget(self._min_button)
-        controls_layout.addWidget(self._max_button)
-
-        layout.addWidget(controls, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
-        layout.addWidget(self.header_title, 0, QtCore.Qt.AlignmentFlag.AlignVCenter)
-        layout.addStretch()
-        return bar
-
-    def _make_chrome_button(
-        self, role: str, callback, tooltip: str | None = None
-    ) -> QtWidgets.QToolButton:
-        button = QtWidgets.QToolButton()
-        button.setObjectName("ChromeDot")
-        button.setProperty("dot", role)
-        button.setAutoRaise(True)
-        button.setFixedSize(12, 12)
-        button.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
-        if tooltip:
-            button.setToolTip(tooltip)
-        button.clicked.connect(callback)
-        self._chrome_buttons.append(button)
-        return button
+ 
 
     def _apply_saved_window_size(self) -> None:
         size = self.settings.get("window_size", {}) or {}
@@ -186,170 +136,6 @@ class MainWindow(QtWidgets.QMainWindow):
             "window_size",
             {"width": int(size.width()), "height": int(size.height())},
         )
-
-    def _toggle_maximize(self) -> None:
-        if self.isMaximized():
-            self.showNormal()
-        else:
-            self.showMaximized()
-
-    def _event_global_pos(self, event: QtGui.QMouseEvent) -> QtCore.QPoint:
-        if hasattr(event, "globalPosition"):
-            return event.globalPosition().toPoint()
-        return event.globalPos()
-
-    def _hit_test_edges(self, pos: QtCore.QPoint) -> str | None:
-        if self.isMaximized() or self.isFullScreen():
-            return None
-        margin = self._RESIZE_MARGIN
-        width = self.rect().width()
-        height = self.rect().height()
-        left = pos.x() <= margin
-        right = pos.x() >= width - margin
-        top = pos.y() <= margin
-        bottom = pos.y() >= height - margin
-        if top and left:
-            return "top_left"
-        if top and right:
-            return "top_right"
-        if bottom and left:
-            return "bottom_left"
-        if bottom and right:
-            return "bottom_right"
-        if left:
-            return "left"
-        if right:
-            return "right"
-        if top:
-            return "top"
-        if bottom:
-            return "bottom"
-        return None
-
-    def _update_resize_cursor(self, edges: str | None) -> None:
-        if self._resize_active or self._drag_active:
-            return
-        if edges is None:
-            self.unsetCursor()
-            return
-        if edges in ("left", "right"):
-            shape = QtCore.Qt.CursorShape.SizeHorCursor
-        elif edges in ("top", "bottom"):
-            shape = QtCore.Qt.CursorShape.SizeVerCursor
-        elif edges in ("top_left", "bottom_right"):
-            shape = QtCore.Qt.CursorShape.SizeFDiagCursor
-        elif edges in ("top_right", "bottom_left"):
-            shape = QtCore.Qt.CursorShape.SizeBDiagCursor
-        else:
-            self.unsetCursor()
-            return
-        self.setCursor(QtGui.QCursor(shape))
-
-    def _perform_resize(self, global_pos: QtCore.QPoint) -> None:
-        if not self._resize_edges:
-            return
-        delta = global_pos - self._resize_start_pos
-        geom = QtCore.QRect(self._resize_start_geom)
-        if "left" in self._resize_edges:
-            geom.setLeft(geom.left() + delta.x())
-        if "right" in self._resize_edges:
-            geom.setRight(geom.right() + delta.x())
-        if "top" in self._resize_edges:
-            geom.setTop(geom.top() + delta.y())
-        if "bottom" in self._resize_edges:
-            geom.setBottom(geom.bottom() + delta.y())
-
-        min_w = self.minimumWidth()
-        min_h = self.minimumHeight()
-        if geom.width() < min_w:
-            if "left" in self._resize_edges:
-                geom.setLeft(geom.right() - min_w + 1)
-            else:
-                geom.setRight(geom.left() + min_w - 1)
-        if geom.height() < min_h:
-            if "top" in self._resize_edges:
-                geom.setTop(geom.bottom() - min_h + 1)
-            else:
-                geom.setBottom(geom.top() + min_h - 1)
-        self.setGeometry(geom)
-
-    def _is_chrome_button(self, obj: QtCore.QObject) -> bool:
-        for button in self._chrome_buttons:
-            if obj is button:
-                return True
-        return False
-
-    def _is_drag_area(self, obj: QtCore.QObject) -> bool:
-        if self.isMaximized() or self.isFullScreen():
-            return False
-        if self.chrome_bar is None:
-            return False
-        if obj is self.chrome_bar or self.chrome_bar.isAncestorOf(obj):
-            return not self._is_chrome_button(obj)
-        return False
-
-    def eventFilter(self, obj: QtCore.QObject, event: QtCore.QEvent) -> bool:
-        if not isinstance(obj, QtWidgets.QWidget) or obj.window() is not self:
-            return super().eventFilter(obj, event)
-
-        if event.type() == QtCore.QEvent.Type.MouseButtonDblClick:
-            if (
-                isinstance(event, QtGui.QMouseEvent)
-                and event.button() == QtCore.Qt.MouseButton.LeftButton
-                and self._is_drag_area(obj)
-            ):
-                self._toggle_maximize()
-                return True
-
-        if event.type() == QtCore.QEvent.Type.MouseButtonPress:
-            if not isinstance(event, QtGui.QMouseEvent):
-                return super().eventFilter(obj, event)
-            if event.button() == QtCore.Qt.MouseButton.LeftButton:
-                if self._is_chrome_button(obj):
-                    return super().eventFilter(obj, event)
-                global_pos = self._event_global_pos(event)
-                pos = self.mapFromGlobal(global_pos)
-                edges = self._hit_test_edges(pos)
-                if edges:
-                    self._resize_active = True
-                    self._resize_edges = edges
-                    self._resize_start_pos = global_pos
-                    self._resize_start_geom = self.geometry()
-                    return True
-                if self._is_drag_area(obj):
-                    self._drag_active = True
-                    self._drag_offset = global_pos - self.frameGeometry().topLeft()
-                    return True
-
-        if event.type() == QtCore.QEvent.Type.MouseMove:
-            if not isinstance(event, QtGui.QMouseEvent):
-                return super().eventFilter(obj, event)
-            global_pos = self._event_global_pos(event)
-            pos = self.mapFromGlobal(global_pos)
-            if self._resize_active:
-                self._perform_resize(global_pos)
-                return True
-            if self._drag_active:
-                self.move(global_pos - self._drag_offset)
-                return True
-            self._update_resize_cursor(self._hit_test_edges(pos))
-
-        if event.type() == QtCore.QEvent.Type.MouseButtonRelease:
-            if not isinstance(event, QtGui.QMouseEvent):
-                return super().eventFilter(obj, event)
-            if event.button() == QtCore.Qt.MouseButton.LeftButton:
-                if self._resize_active or self._drag_active:
-                    self._resize_active = False
-                    self._drag_active = False
-                    self._resize_edges = None
-                    self._update_resize_cursor(None)
-                    return True
-
-        if event.type() == QtCore.QEvent.Type.Leave:
-            if not self._resize_active and not self._drag_active:
-                self._update_resize_cursor(None)
-
-        return super().eventFilter(obj, event)
 
     def resizeEvent(self, event: QtGui.QResizeEvent) -> None:
         super().resizeEvent(event)
